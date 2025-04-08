2a3ea77ee8b18da360f0f3b751b4bd62a9c8db41
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-04-09 01:20:03 +0600

    added doctor controller, adminroute doctor route & test by thunder client

--- Doctor-Backend/controllers/adminController.js
+++ Doctor-Backend/controllers/adminController.js
@@ -1,9 +1,11 @@
 
-
 // API for adding doctor
-const addDoctor = async()=> {
+const addDoctor = async(req, res)=> {
     try {
         const {name, email, password, speciality, degree, experience, about, fees, address} = req.body
+        const imageFile = req.file
+
+        console.log({name, email, password, speciality, degree, experience, about, fees, address},imageFile)
     } catch (error) {
         
     }
