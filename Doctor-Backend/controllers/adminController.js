7229b343c8cd7e75b7e517fd5b6dd26ba76f3063
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-04-09 23:48:39 +0600

    added bcrypt hashed password method

--- Doctor-Backend/controllers/adminController.js
+++ Doctor-Backend/controllers/adminController.js
@@ -1,3 +1,6 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+
 
 // API for adding doctor
 const addDoctor = async(req, res)=> {
@@ -5,9 +8,25 @@ const addDoctor = async(req, res)=> {
         const {name, email, password, speciality, degree, experience, about, fees, address} = req.body
         const imageFile = req.file
 
+        // checking for all data to addDoctor
+
         if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
             return res.json({success:false,message:'Missing Details'})
         }
+
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return res.json({success:false,message:'Please enter a valid email'})
+        }
+
+        // validating strong password
+        if (password.length < 8) {
+            return res.json({success:false,message:'Please enter a strong password'})
+        }
+
+        // hashing doctor password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
 
     } catch (error) {
         
