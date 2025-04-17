24416c8ac8e207a2d8d78c28399ae3cc531fe460
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-04-18 01:01:15 +0600

    admin panel project started and login ui ready for admin and doctor

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css';
+import Login from './pages/Login';
+
+
+export default function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
---
+++ admin/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#5F6FFF'
+      }
+    },
+  },
+  plugins: [],
+}
