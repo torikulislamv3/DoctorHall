a1b1857017c7b68f8c833570ff09abc09b917c12
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-04-24 00:12:47 +0600

    navbar updated for profile picture

--- Doctor-Frontend/src/components/Navbar.jsx
+++ Doctor-Frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../context/AppContext';
 export default function Navbar() {
 
     const navigate = useNavigate();
-    const {token, setToken} = useContext(AppContext)
+    const {token, setToken, userData} = useContext(AppContext)
 
     const [showMenu, setShowMenu] = useState(false);
 
@@ -40,8 +40,10 @@ export default function Navbar() {
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
-        {token ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+        {token && userData
+         ? 
+         <div className='flex items-center gap-2 cursor-pointer group relative'>
+            <img className='w-8 rounded-full' src={userData.image} alt="" />
             <img className='w-2.5' src={assets.dropdown_icon} alt="" />
             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
